40d24859b6fd0752ad123f37b8d52162b6c47dd7
Harry Brown <harrybrown98@gmail.com>
2019-09-05 15:07:36 -0700

    rewrote assert with colors

--- tests/test_common.h
+++ tests/test_common.h
@@ -2,24 +2,36 @@
 #define HASHMAP_TEST_COMMON_H
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 #include <assert.h>
 #include <string.h>
 #include <sys/types.h>
 
-    typedef void (*testfunc)(void);
+typedef void (*testfunc)(void);
+
+#ifdef assert
+#undef assert
+#endif
+
+// because i like pretty colors :)
+#define assert(exp)                                                                               \
+    ((exp) ? (void)(0)                                                                            \
+           : __assert_fail("\033[0;31m" #exp "\033[0m", "\033[0;31m" __FILE__ "\033[0m",          \
+                           __LINE__, __ASSERT_FUNCTION))
 
 /**
  * compares two string arrays that can be out of order
  */
 #define ASSERT_STR_ARR_EQ(A1, A2, LEN)                                                            \
     {                                                                                             \
-        for (i = 0; i < LEN; i++) {                                                               \
-            for (k = 0; k < LEN; k++) {                                                           \
-                if (strcmp(A1[i], A2[k]) == 0) {                                                  \
+        for (i = 0; i < LEN; i++)                                                                 \
+        {                                                                                         \
+            for (k = 0; k < LEN; k++)                                                             \
+            {                                                                                     \
+                if (strcmp(A1[i], A2[k]) == 0)                                                    \
+                {                                                                                 \
                     goto Found;                                                                   \
                 }                                                                                 \
             }                                                                                     \
@@ -28,16 +40,16 @@ extern "C"
         }                                                                                         \
     }
 
-    extern char* randstring(size_t);
-    extern char** rand_keys(size_t n);
-    extern void free_string_arr(char** arr, int len);
-    extern double benchmark(testfunc);
-    extern double nbenchmark(testfunc, int);
-    extern void Benchmark(const char*, testfunc);
-    extern void AverageBenchmark(const char*, testfunc, size_t);
-    extern void Benchmarkf(const char*, testfunc, const char*);
-    extern void nBenchmark(const char*, testfunc, size_t);
-    extern int Run(testfunc*, int);
+extern char* randstring(size_t);
+extern char** rand_keys(size_t n);
+extern void free_string_arr(char** arr, int len);
+extern double benchmark(testfunc);
+extern double nbenchmark(testfunc, int);
+extern void Benchmark(const char*, testfunc);
+extern void AverageBenchmark(const char*, testfunc, size_t);
+extern void Benchmarkf(const char*, testfunc, const char*);
+extern void nBenchmark(const char*, testfunc, size_t);
+extern int Run(testfunc*, int);
 
 #ifdef __cplusplus
 }
