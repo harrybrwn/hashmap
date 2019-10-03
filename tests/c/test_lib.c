f28960b6f63e597238c31acbd1931c5f96e4ab5d
Harry Brown <harrybrown98@gmail.com>
2019-10-03 14:14:21 -0700

    playing around with lib test for the shits

--- tests/c/test_lib.c
+++ tests/c/test_lib.c
@@ -4,8 +4,11 @@
  */
 
 #include <hashmap.h>
+#include <map_iter.h>
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char* keys[] = { "one", "two", "three", "apple", "orange" };
 
@@ -16,8 +19,15 @@ struct address
     int num;
     char* street;
     char* city;
-    char state[2];
+    char* state;
 };
+
+#define INIT_ADDR(addr, N, S, C, ST)                                                              \
+    memset(&addr, 0, sizeof(struct address));                                                     \
+    addr.num = N;                                                                                 \
+    addr.street = S;                                                                              \
+    addr.city = C;                                                                                \
+    addr.state = ST;
 
 #define setup __attribute__((constructor))
 #define teardown __attribute__((destructor))
@@ -26,8 +36,11 @@ static Map* m;
 
 void add_jimbo(void)
 {
-    char* name = "jimbo";
-    struct address jimbos_house = { 1234, "Maple St.", "San Fransisco", "CA" };
+    char* name = malloc(6);
+    strncpy(name, "jimbo", 6);
+
+    struct address jimbos_house;
+    INIT_ADDR(jimbos_house, 1234, "Maple St.", "San Fransisco", "CA");
     map_key_put(m, (struct key){ &jimbos_house, sizeof(struct address) }, name);
 }
 
@@ -36,6 +49,7 @@ setup void start(void)
     m = create_map(127);
     add_jimbo();
 }
+
 teardown void end(void)
 {
     map_free(m);
@@ -57,18 +71,32 @@ int main(void)
             printf("wrong value!\n");
     }
 
-    struct key addr_key = { &((struct address){ 1234, "Maple St.", "San Fransisco", "CA" }),
-                            sizeof(struct address) };
+    struct address house;
+    INIT_ADDR(house, 1234, "Maple St.", "San Fransisco", "CA");
+    struct key addr_key = { &house, sizeof(struct address) };
 
     char* name = map_key_get(m, addr_key);
     if (name == NULL)
     {
-        printf("could not find owners name\n");
+        printf("could not find owners name!!!!\n");
+        return 1;
     }
     else
     {
         printf("the owner's name is %s\n", name);
     }
 
-    // MapIterator* it = map_iter(m);
+    MapIterator* it;
+
+    struct node* n;
+    it = map_iter(m);
+    while (!iter_done(it))
+    {
+        n = iter_next(it);
+        printf("%lu ", n->hash);
+        printf("%s ", n->key);
+        printf("\n");
+    }
+    free(it);
+    free(name);
 }
